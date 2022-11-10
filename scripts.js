c3c89eea0eb6dc65dedf1ef83a86913005899d70
Vesko <veskoap@gmail.com>
2022-11-10 06:08:37 -0500

    Updated game logic & added a prompt

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
+let playerScore = 0, computerScore = 0, round = 0;
+let playerSelection = "";
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
-
     switch (randomNumber) {
         case 0:
             return 'rock';
@@ -13,11 +15,9 @@ function getComputerChoice() {
     }
 }  
 
-
-let playerScore = 0;
-let computerScore = 0;
-
-function playRound(playerSelection = getComputerChoice(), computerSelection = getComputerChoice()) {
+function playRound(playerSelection, computerSelection = getComputerChoice()) {
+    playerSelection = prompt("rock/paper/scissors");
+    playerSelection = playerSelection.toLowerCase();
 
     if (playerSelection == computerSelection) {
         console.log('Tie!');
@@ -25,7 +25,7 @@ function playRound(playerSelection = getComputerChoice(), computerSelection = ge
         if (computerSelection == 'paper') {
             console.log('Computer Wins! Paper beats rock.' );
             computerScore++;
-        } else {
+        } else { //computerSelection == scissors
             console.log('Player Wins! Rock beats scissors.');
             playerScore++;
         }
@@ -33,27 +33,37 @@ function playRound(playerSelection = getComputerChoice(), computerSelection = ge
         if (computerSelection == 'scissors') {
             console.log('Computer Wins! Scissors beats paper.');
             computerScore++;
-        } else {
+        } else { //computerSelection == rock
             console.log('Player Wins! Paper beats rock.');
             playerScore++;
         }
-    } else { //playerSelection === 'scissors'
+    } else { //playerSelection == scissors
         if (computerSelection == 'rock') {
             console.log('Computer Wins! Rock beats scissors.' );
             computerScore++;
-        } else {
+        } else { //computerSelection == paper
             console.log('Player Wins! Scissors beats paper.');
             playerScore++;
         }
     }
 }
 
-function game() {
-    let round = 0;
+function getWinner() {
+    if (playerScore == computerScore) {
+        console.log("It is a tie!");
+    } else if (playerScore > computerScore) {
+        console.log("Player Wins Game!");
+    } else {
+        console.log("Computer Wins Game!");
+    }
+}
 
+function game() {
     for (let i = 0; i < 5; i++) {
         playRound();
         round++
         console.log("Round: " + round + "| Player Score: " + playerScore + "  Computer Score: " + computerScore); 
      }
+    getWinner();
 }
+
